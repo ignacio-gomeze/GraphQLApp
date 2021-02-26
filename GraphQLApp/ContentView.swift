de5a962db4ffd0c5f04cd7469665e97666654bb5
Ignacio Gómez <ignacio.gomeze@bci.cl>
2021-02-26 10:26:00 -0300

    Add init project

---
+++ GraphQLApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraphQLApp
+//
+//  Created by Ignacio Gómez on 26-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraphQLApp/GraphQLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLAppApp.swift
+//  GraphQLApp
+//
+//  Created by Ignacio Gómez on 26-02-21.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
